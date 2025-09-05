2644f0545d1de79317e6823b61be895c2c9158f3
Michael Naatjes <michael.naatjes87@gmail.com>
2025-09-05 14:49:43 -0400

    ready for development

--- src/Foundation/app.php
+++ src/Foundation/app.php
@@ -2,7 +2,16 @@
 
 	use MVCFrame\Tests\Classes\ServiceContainer;
 
+	/**
+	 * Check for app() method
+	 */
 	if(!function_exists("app")){
+		/**
+		 * Helper function linking ServiceContainer to app() interface
+		 *
+		 * @param string $key
+		 * @return void
+		 */
 	    function app(string $key){
 	        return ServiceContainer::getInstance()->resolve($key);
 	    }
