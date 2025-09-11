4fb47da69dfd3e5f4ddf0142c5a931e57936eae1
Michael Naatjes <michael.naatjes87@gmail.com>
2025-09-11 18:19:11 -0400

    working test environment with docker for core/ and skeleton/ directories

---
+++ skeleton/bootstrap/app.php
@@ -0,0 +1,10 @@
+<?php
+
+	/**
+	 * Main Application Bootstrap file for MVC Framework Testing
+	 * - Instantiate Application Class
+	 * - Assign dirname(__DIR__) as parameter or .env variable
+	 */
+	$app = new \MVCFrame\Foundation\Application(dirname(__DIR__));
+
+?>
